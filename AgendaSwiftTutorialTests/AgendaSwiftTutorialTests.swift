2189d74d97378cb48837d0bd60d6dab2070baaa7
iXavy <development@ixavy.com>
2015-11-03 19:27:36 +0100

    Initial Commit

---
+++ AgendaSwiftTutorialTests/AgendaSwiftTutorialTests.swift
@@ -0,0 +1,36 @@
+//
+//  AgendaSwiftTutorialTests.swift
+//  AgendaSwiftTutorialTests
+//
+//  Created by Javier  on 3/11/15.
+//  Copyright © 2015 ixavy. All rights reserved.
+//
+
+import XCTest
+@testable import AgendaSwiftTutorial
+
+class AgendaSwiftTutorialTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
